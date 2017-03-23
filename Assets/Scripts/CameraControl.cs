1d4fc7d4b98e68d6732df7b3376f32c82a43b262
Enze Mao <Enze@Enzes-Mac-mini.local>
2017-03-23 00:07:35 +0000

    init: finished player basic action walk and attack, done basic camera control and import a simple test map made by tilemap

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	//target that camera need to follow
+	[SerializeField]
+	private GameObject followTarget;
+	[SerializeField]
+	private float cameraSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		HandleCameraMovement ();	
+	}
+
+	//camera follow a specific gameobject with a preset speed
+	void HandleCameraMovement () {
+		Vector3 fromPos = this.transform.position;
+		Vector3 toPos = new Vector3 (followTarget.transform.position.x
+			, followTarget.transform.position.y
+			, fromPos.z);
+		this.transform.position = Vector3.Lerp (fromPos, toPos, cameraSpeed);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int moveSpeed;
+
+	private Animator playerAnimator;
+	private Rigidbody2D playerBody;
+	private float hDir; 
+	private float vDir; 
+	// Use this for initialization
+	void Start () {
+		playerAnimator = GetComponent<Animator> ();
+		playerBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hDir = Input.GetAxisRaw ("Horizontal");
+		vDir = Input.GetAxisRaw ("Vertical");
+		HandleMovement ();	
+		HandleAttack ();
+	}
+
+	//move vertically or horizontally based on player's axis raw
+	void HandleMovement () {
+		Vector2 moveDir = new Vector2 (hDir, vDir);
+		if (moveDir != Vector2.zero) {
+			playerAnimator.SetBool ("isWalking", true);
+			playerAnimator.SetFloat ("hSpeed", moveDir.x);
+			playerAnimator.SetFloat ("vSpeed", moveDir.y);
+		} else {
+			playerAnimator.SetBool ("isWalking", false);
+		}
+		//change position
+		transform.Translate (new Vector3 ((moveDir.x * moveSpeed * Time.deltaTime)
+			, (moveDir.y * moveSpeed * Time.deltaTime)
+			, 0f));
+	}
+
+	//player attack based on facing direction attack control: J
+	void HandleAttack () {
+		if (Input.GetKeyDown(KeyCode.J)) {
+			playerAnimator.SetTrigger ("isAttacking");
+		} 
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		print ("hit");
+		if (coll.gameObject.tag == "Map")
+			coll.gameObject.SendMessage("ApplyDamage", 10);
+	}
+}
